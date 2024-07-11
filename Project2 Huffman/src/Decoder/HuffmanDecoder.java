b32baf70da69cf4154c67a5d5d054b15bcba35ed
aprakash3 <135915407+aprakash3@users.noreply.github.com>
2024-07-10 21:51:33 -0700

    Uploading Project 2

---
+++ Project2 Huffman/src/Decoder/HuffmanDecoder.java
@@ -0,0 +1,12 @@
+package Decoder;
+
+import Utils.*;
+import java.io.IOException;
+
+public class HuffmanDecoder {
+    // Define any helper methods up here.
+
+    public static void decodeFile(String compressedFilePath, String outputFilePath){
+        // TODO
+    }
+}
---
+++ Project2 Huffman/src/Encoder/HuffmanEncoder.java
@@ -0,0 +1,15 @@
+package Encoder;
+
+import java.io.IOException;
+import java.util.*;
+
+import Decoder.HuffmanDecoder;
+import Utils.*;
+
+public class HuffmanEncoder {
+    // Define any helper methods up here.
+    public static void encodeFile(String inputFilePath, String outputFilePath){
+        // TODO
+        // Put all orchestration logic here.
+    }
+}
